11aba2a0b9c4b6bdc0ad997cfcb93115b9d638a6
Caleb P. Charpentier <calebc22@vt.edu>
2023-09-04 20:57:26 -0400

    organize __init__.py

--- TraitBlender/__init__.py
+++ TraitBlender/__init__.py
@@ -33,10 +33,69 @@ bl_info = {
 
 def register():
     
+    ##Register Operators
+    ###BackGroundPlanes
+    bpy.utils.register_class(ImportBackgroundImageOperator)
+    bpy.utils.register_class(CreateBackgroundImageMeshOperator)
+    bpy.utils.register_class(ToggleBackgroundPlanesOperator)
+    bpy.utils.register_class(HideBackgroundPlanesOperator)
+    bpy.utils.register_class(BackgroundControls)
+    bpy.utils.register_class(ScaleBackgroundPlanesOperator)
+    bpy.utils.register_class(CallUpdateBackgroundPlaneDistanceOperator)
+
+    ###CamerasRendering
+    bpy.utils.register_class(ToggleCamerasOperator)
+    bpy.utils.register_class(HideCamerasOperator)
+    bpy.utils.register_class(CameraControls)
+    bpy.utils.register_class(RenderAllCamerasOperator)
+    bpy.utils.register_class(SetCameraViewOperator)
+    bpy.utils.register_class(SelectRenderDirectoryOperator)
+
+    ###DeleteAllObjecctsOperator
     bpy.utils.register_class(DeleteAllObjectsOperator)
-    bpy.utils.register_class(ExportControlsPropertyGroup)
-    bpy.types.Scene.export_controls_property = bpy.props.PointerProperty(type=ExportControlsPropertyGroup)
+
+    ###ExportActiveObjectOperator
     bpy.utils.register_class(ExportActiveObjectOperator)
+
+    ###ExportSettingsOperator
+    bpy.utils.register_class(ExportSettingsOperator)
+
+    ###MeshGeneratingFunction
+    bpy.utils.register_class(OpenMeshFunctionFileBrowserOperator)
+    bpy.utils.register_class(ImportMakeMeshFunctionPathOperator)
+    bpy.utils.register_class(ExecuteStoredFunctionOperator)
+
+    ###Segmentation
+    bpy.utils.register_class(SegmentationControls)
+    bpy.utils.register_class(SegmentationControlsProperty)
+    bpy.utils.register_class(SaveVertexGroupsToCSVOperator)
+    bpy.utils.register_class(LoadVertexGroupsFromCSVOperator)
+
+    ###Suns
+    bpy.utils.register_class(SunControls)    
+    bpy.utils.register_class(ToggleSunsOperator)
+    bpy.utils.register_class(HideSunsOperator)
+    bpy.utils.register_class(SunControlsPanel) 
+    bpy.utils.register_class(UpdateSunStrengthOperator)
+
+    ###TraitBlenderPanel
+    bpy.utils.register_class(TraitBlenderPanel)
+
+    ###TraitDataCSV
+    bpy.utils.register_class(CSVLabelProperties) 
+    bpy.utils.register_class(ImportCSVOperator)
+    bpy.utils.register_class(ExecuteWithSelectedCSVRowOperator)
+
+    ###WorldBackground
+    bpy.utils.register_class(WorldBackgroundControls)
+    bpy.utils.register_class(ChangeWorldBackgroundColor)
+    bpy.utils.register_class(ExportControlsPropertyGroup)   
+
+    
+    
+    #Register Properties
+    bpy.types.Scene.export_controls_property = bpy.props.PointerProperty(type=ExportControlsPropertyGroup)
+    
     bpy.types.Scene.export_directory = bpy.props.StringProperty(
         name="Export Directory",
         description="Directory to export the 3D object",
@@ -51,16 +110,15 @@ def register():
         default='.obj'
     )
 
-    bpy.utils.register_class(ExecuteWithSelectedCSVRowOperator)
-    bpy.utils.register_class(CSVLabelProperties)
     bpy.types.Scene.csv_label_props = bpy.props.PointerProperty(type=CSVLabelProperties)
-    bpy.utils.register_class(ImportCSVOperator)
+
     bpy.types.Scene.csv_file_path = bpy.props.StringProperty(
         name="CSV File Path",
         description="Path to the CSV file",
         default="",
         subtype='FILE_PATH'
     )
+    
     bpy.types.Scene.csv_labels = bpy.props.StringProperty(
         name="CSV Labels",
         description="Labels imported from CSV",
@@ -73,45 +131,23 @@ def register():
             )
         )
     )
-    bpy.utils.register_class(OpenMeshFunctionFileBrowserOperator)
-    bpy.utils.register_class(ImportMakeMeshFunctionPathOperator)
-    bpy.utils.register_class(ExecuteStoredFunctionOperator)
+
     bpy.types.Scene.make_mesh_function_path = bpy.props.StringProperty(
         name="Make Mesh Function Path",
         description="File path for the make_mesh function",
         default="",
     )
-    bpy.utils.register_class(ImportBackgroundImageOperator)
-    bpy.utils.register_class(ExportSettingsOperator)
-    bpy.utils.register_class(WorldBackgroundControls)
+
     bpy.types.Scene.world_background_controls = bpy.props.PointerProperty(type=WorldBackgroundControls)
-    bpy.utils.register_class(ChangeWorldBackgroundColor)
-    bpy.utils.register_class(ToggleCamerasOperator)
-    bpy.utils.register_class(TraitBlenderPanel)
-    bpy.utils.register_class(CameraControls)
-    bpy.utils.register_class(HideCamerasOperator)
-    bpy.utils.register_class(SelectRenderDirectoryOperator)
-    bpy.utils.register_class(RenderAllCamerasOperator)
-    bpy.utils.register_class(ToggleBackgroundPlanesOperator)
-    bpy.utils.register_class(ScaleBackgroundPlanesOperator)
-    bpy.utils.register_class(HideBackgroundPlanesOperator)
-    bpy.utils.register_class(SegmentationControls)
-    bpy.utils.register_class(SegmentationControlsProperty)
-    bpy.utils.register_class(SaveVertexGroupsToCSVOperator)
-    bpy.utils.register_class(LoadVertexGroupsFromCSVOperator)
-    bpy.utils.register_class(ToggleSunsOperator)
-    bpy.utils.register_class(HideSunsOperator)
-    bpy.utils.register_class(SunControlsPanel)
-    bpy.utils.register_class(SetCameraViewOperator)
-    bpy.utils.register_class(BackgroundControls)
+
     bpy.types.Scene.background_controls = bpy.props.PointerProperty(type=BackgroundControls)
-    bpy.utils.register_class(CallUpdateBackgroundPlaneDistanceOperator)
+
     bpy.types.Scene.segmentation_controls_property = bpy.props.PointerProperty(type=SegmentationControlsProperty)
     bpy.types.Scene.lights_controls = bpy.props.BoolProperty(default=False)
-    bpy.utils.register_class(SunControls)
+
     bpy.types.Scene.sun_controls = bpy.props.PointerProperty(type=SunControls)
     bpy.types.Scene.sun_lamp_controls = bpy.props.BoolProperty(name="Sun Lamp Controls", default=False)
-    bpy.utils.register_class(UpdateSunStrengthOperator)
+
     bpy.types.Scene.sun_strength = bpy.props.FloatProperty(
         name="Sun Strength",
         description="Strength of the sun lamps",
@@ -133,7 +169,7 @@ def register():
     bpy.types.Scene.background_controls = bpy.props.PointerProperty(type=BackgroundControls)
     bpy.types.Scene.background_image_reference = bpy.props.StringProperty(default="")
 
-    bpy.utils.register_class(CreateBackgroundImageMeshOperator)
+
     bpy.types.VIEW3D_MT_mesh_add.append(mesh_menu_func)
 
     bpy.types.Scene.camera_controls = bpy.props.PointerProperty(type=CameraControls)
@@ -180,69 +216,90 @@ def register():
     
 def unregister():
     
-    bpy.utils.unregister_class(DeleteAllObjectsOperator)
-    del bpy.types.Scene.export_controls_property
-    bpy.utils.unregister_class(ExportControlsPropertyGroup)
-    bpy.utils.unregister_class(ExportActiveObjectOperator)
-    del bpy.types.Scene.export_directory
-    del bpy.types.Scene.export_format
-    bpy.utils.unregister_class(ExecuteWithSelectedCSVRowOperator)
-    bpy.utils.unregister_class(CSVLabelProperties)
-    del bpy.types.Scene.csv_label_props
-    bpy.utils.unregister_class(ImportCSVOperator)
-    del bpy.types.Scene.csv_file_path
-    del bpy.types.Scene.csv_labels
-    if hasattr(bpy.types.Scene, 'csv_label_enum'):
-        del bpy.types.Scene.csv_label_enum
-    bpy.utils.unregister_class(OpenMeshFunctionFileBrowserOperator)
-    bpy.utils.unregister_class(ExecuteStoredFunctionOperator)
-    bpy.utils.unregister_class(ImportMakeMeshFunctionPathOperator)
-    del bpy.types.Scene.make_mesh_function_path
-    bpy.utils.unregister_class(ImportMakeMeshFunctionOperator)
+    ##Unregister operators
+    ###BackGroundPlanes
     bpy.utils.unregister_class(ImportBackgroundImageOperator)
-    bpy.utils.unregister_class(ExportSettingsOperator)
-    bpy.utils.unregister_class(WorldBackgroundControls)
-    del bpy.types.Scene.world_background_controls
-    bpy.utils.unregister_class(ToggleCamerasOperator)
-    bpy.utils.unregister_class(TraitBlenderPanel)
-    bpy.utils.unregister_class(CameraControls)
+    bpy.utils.unregister_class(CreateBackgroundImageMeshOperator)    
+    bpy.utils.unregister_class(ToggleBackgroundPlanesOperator)    
+    bpy.utils.unregister_class(HideBackgroundPlanesOperator)    
+    bpy.utils.unregister_class(BackgroundControls)    
+    bpy.utils.unregister_class(ScaleBackgroundPlanesOperator)    
+    bpy.utils.unregister_class(CallUpdateBackgroundPlaneDistanceOperator)    
+    
+    ###CamerasRendering
+    bpy.utils.unregister_class(ToggleCamerasOperator)    
     bpy.utils.unregister_class(HideCamerasOperator)
-    bpy.utils.unregister_class(SelectRenderDirectoryOperator)
+    bpy.utils.unregister_class(CameraControls)
     bpy.utils.unregister_class(RenderAllCamerasOperator)
-    bpy.utils.unregister_class(ToggleBackgroundPlanesOperator)
-    bpy.utils.unregister_class(ScaleBackgroundPlanesOperator)
-    bpy.utils.unregister_class(HideBackgroundPlanesOperator)
-    bpy.utils.unregister_class(SegmentationControlsProperty)
+    bpy.utils.unregister_class(SetCameraViewOperator)
+    bpy.utils.unregister_class(SelectRenderDirectoryOperator)    
+    
+    ###DeleteAllObjectsOperator
+    bpy.utils.unregister_class(DeleteAllObjectsOperator)
+    
+    ###ExportActiveObjectOperator
+    bpy.utils.unregister_class(ExportActiveObjectOperator)
+     
+    ###ExportSettingsOperator 
+    bpy.utils.unregister_class(ExportSettingsOperator)
+    
+    ###MeshGeneratingFunction
+    bpy.utils.unregister_class(OpenMeshFunctionFileBrowserOperator)
+    bpy.utils.unregister_class(ImportMakeMeshFunctionPathOperator)
+    bpy.utils.unregister_class(ExecuteStoredFunctionOperator)    
+    
+    ###Segmentation
+    bpy.utils.unregister_class(SegmentationControls)
+    bpy.utils.unregister_class(SegmentationControlsProperty)    
+    bpy.utils.unregister_class(SaveVertexGroupsToCSVOperator)    
+    bpy.utils.unregister_class(LoadVertexGroupsFromCSVOperator)    
+    
+    ###Suns
+    bpy.utils.unregister_class(SunControls)
     bpy.utils.unregister_class(ToggleSunsOperator)
     bpy.utils.unregister_class(HideSunsOperator)
     bpy.utils.unregister_class(SunControlsPanel)
-    bpy.utils.unregister_class(LoadVertexGroupsFromCSVOperator)
-    bpy.utils.unregister_class(SaveVertexGroupsToCSVOperator)
-    bpy.utils.unregister_class(SetCameraViewOperator)
-    bpy.utils.unregister_class(ChangeBackgroundColor)
-    bpy.utils.unregister_class(BackgroundControls)
-    del bpy.types.Scene.background_controls
-    bpy.utils.unregister_class(CallUpdateBackgroundPlaneDistanceOperator)
-    del bpy.types.Scene.segmentation_controls_property
-    del bpy.types.Scene.lights_controls
-    bpy.utils.unregister_class(SunControls)
+    bpy.utils.unregister_class(UpdateSunStrengthOperator)    
+    
+    ###TraitBlenderPanel
+    bpy.utils.unregister_class(TraitBlenderPanel)
+    
+    
+    ###TraitDataCSV    
+    bpy.utils.unregister_class(CSVLabelProperties)
+    bpy.utils.unregister_class(ImportCSVOperator)    
+    bpy.utils.unregister_class(ExecuteWithSelectedCSVRowOperator)    
+    
+    ###WorldBackground
+    bpy.utils.unregister_class(WorldBackgroundControls)
+    bpy.utils.unregister_class(ChangeWorldBackgroundColor)
+    bpy.utils.unregister_class(ExportControlsPropertyGroup)
+    
+    
+    
+    #unregister properties
+    bpy.types.VIEW3D_MT_mesh_add.remove(mesh_menu_func)
+    del bpy.types.Scene.world_background_controls 
+    del bpy.types.Scene.make_mesh_function_path 
+    del bpy.types.Scene.csv_label_props
+    del bpy.types.Scene.export_controls_property
+    del bpy.types.Scene.export_directory
+    del bpy.types.Scene.export_format
     del bpy.types.Scene.sun_controls
-    bpy.utils.unregister_class(UpdateSunStrengthOperator)
+    del bpy.types.Scene.segmentation_controls_property
+    del bpy.types.Scene.lights_controls 
+    del bpy.types.Scene.background_controls
     del bpy.types.Scene.sun_strength
-
-
-    bpy.utils.unregister_class(ImportBackgroundImageOperator)
-    bpy.utils.unregister_class(BackgroundControls)
     del bpy.types.Scene.background_controls
     del bpy.types.Scene.background_image_reference
-    
-    bpy.utils.unregister_class(CreateBackgroundImageMeshOperator)
-    bpy.types.VIEW3D_MT_mesh_add.remove(mesh_menu_func)
- 
     del bpy.types.Scene.traitblender_settings
     del bpy.types.Scene.place_cameras_distance
     del bpy.types.Scene.camera_controls
     del bpy.types.Scene.render_output_directory
+    del bpy.types.Scene.csv_file_path
+    del bpy.types.Scene.csv_labels
+    if hasattr(bpy.types.Scene, 'csv_label_enum'):
+        del bpy.types.Scene.csv_label_enum
 
     
     
