f65cd41f7bd9c6dddf8792de42b264e365b506df
Caleb P. Charpentier <calebc22@vt.edu>
2023-09-12 14:37:31 -0400

    updating the generate_dataset_updated.py

--- generate_dataset_updated.py
+++ generate_dataset_updated.py
@@ -1,24 +1,37 @@
 
 import bpy
+import json
 
-
+## read in mesh function and trait csv
 make_mesh_function_path = ""
-csv_file_path = ""
+csv_file_path = "D://TraitBlender/TraitBlender/Examples/Data/snails.csv"
+json_file_path = "C://Users/caleb/Downloads/traitblender_settings.json"
 
-wc_red, wc_green, wc_blue, wc_alpha = [1.0, 1.0, 1.0, 1.0]
-background_plane_image_path = ""
-background_plane_distance = 10.00
-bg_scale_x, bg_scale_y, bg_scale_z = [.5, 1.0, 1.0]
-sun_strength = 1.0
-place_cameras_distance = 10.00
-camera_width, camera_height = [1080, 1080]
-focal_length = 50.00
-render_cameras = "camera.top,camera.bottom,camera.right,camera.left,camera.front,camera.back"
-render_output_directory = ""
 
-obj_export_format = ".obj"
-obj_export_directory = ""
+def load_settings_from_json(json_path):
+    with open(json_path, 'r') as f:
+        settings = json.load(f)
+    return settings
 
+
+# Path to the JSON file containing the settings
+settings = load_settings_from_json(json_file_path)
+
+# Extracting variables from the loaded settings
+wc_red, wc_green, wc_blue, wc_alpha = settings["World Background Controls"]["wc_colors"]
+
+if settings["Background Controls"]["background_plane_image_path"] != "None":
+    bpy.ops.traitblender.import_background_image(filepath=settings["Background Controls"]["background_plane_image_path"])
+    
+background_plane_distance = settings["Background Controls"]["background_plane_distance"]
+bg_scale_x, bg_scale_y, bg_scale_z = settings["Background Controls"]["bg_scales"]
+sun_strength = settings["Lights"]["sun_strength"]
+place_cameras_distance = settings["Camera Controls"]["place_cameras_distance"]
+camera_width, camera_height = settings["Camera Controls"]["camera_width_height"]
+focal_length = settings["Camera Controls"]["focal_length"]
+render_output_directory = settings["Camera Controls"]["render_output_directory"]
+obj_export_directory = settings["obj_export_directory"]
+export_format = settings["export_format"]
 
 
 ## Defining the scene
@@ -35,8 +48,6 @@ scene.world_background_controls.blue = wc_blue
 scene.world_background_controls.alpha = wc_alpha
 
 ## set background plane/image properties
-if background_plane_image_path:
-    bpy.ops.traitblender.import_background_image(filepath=background_plane_image_path)
 scene.background_plane_distance = background_plane_distance
 scene.background_controls.plane_scale_x = bg_scale_x
 scene.background_controls.plane_scale_y = bg_scale_y
@@ -53,5 +64,8 @@ scene.camera_controls.focal_length = focal_length
 scene.render_output_directory = render_output_directory
 
 ## set 3D object export properties
-scene.export_format = obj_export_format
+scene.export_format = export_format
 scene.export_directory = obj_export_directory
+
+
+bpy.ops.object.import_csv()
