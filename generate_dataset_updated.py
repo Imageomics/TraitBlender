70ba91dcac2b94cda6d99d8c1cdbf03ebc6dd9a1
Caleb P. Charpentier <calebc22@vt.edu>
2023-09-12 13:04:42 -0400

    work on dataset generating loop

---
+++ generate_dataset_updated.py
@@ -0,0 +1,64 @@
+
+import bpy
+
+
+make_mesh_function_path = ""
+csv_file_path = ""
+
+wc_red, wc_green, wc_blue, wc_alpha = [1.0, 1.0, 1.0, 1.0]
+background_plane_image_path = ""
+background_plane_distance = 10.00
+bg_scale_x, bg_scale_y, bg_scale_z = [1.0, 1.0, 1.0]
+sun_strength = 1.0
+place_cameras_distance = 10.00
+camera_width, camera_height = [1080, 1080]
+focal_length = 50.00
+render_cameras = {"camera.top" : 1, "camera.bottom": 1 ,"camera.right": 1,"camera.left": 1,"camera.front": 1,"camera.back": 1}
+render_output_directory = ""
+
+obj_export_format = ".obj"
+obj_export_directory = ""
+
+
+
+## Defining the scene
+scene = bpy.data.scenes["Scene"]
+
+## setting mesh function and trait data related properties
+scene.csv_file_path = csv_file_path
+scene.make_mesh_function_path = make_mesh_function_path
+
+## set world color properties
+scene.world_background_controls.red = wc_red
+scene.world_background_controls.green = wc_green
+scene.world_background_controls.blue = wc_blue
+scene.world_background_controls.alpha = wc_alpha
+
+## set background plane/image properties
+if background_plane_image_path:
+    bpy.ops.traitblender.import_background_image(filepath=background_plane_image_path)
+scene.background_plane_distance = background_plane_distance
+scene.background_controls.plane_scale_x = bg_scale_x
+scene.background_controls.plane_scale_y = bg_scale_y
+scene.background_controls.plane_scale_z = bg_scale_z
+
+## set the strength of the suns
+scene.sun_strength = sun_strength
+
+## set the camera related properties
+scene.place_cameras_distance = place_cameras_distance
+scene.camera_controls.camera_width = camera_width
+scene.camera_controls.camera_height = camera_height
+scene.camera_controls.focal_length = focal_length
+scene.render_output_directory = render_output_directory
+
+scene.camera_controls.render_camera_top = render_cameras["camera.top"]
+scene.camera_controls.render_camera_bottom = render_cameras["camera.bottom"]
+scene.camera_controls.render_camera_right = render_cameras["camera.right"]
+scene.camera_controls.render_camera_left = render_cameras["camera.left"]
+scene.camera_controls.render_camera_front = render_cameras["camera.front"]
+scene.camera_controls.render_camera_back = render_cameras["camera.back"]
+
+## set 3D object export properties
+scene.export_format = obj_export_format
+scene.export_directory = obj_export_directory
